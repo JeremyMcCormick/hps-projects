2bd710233f4f4fcb6ee23d12549eff12d119f693
Jeremy McCormick <jermccormick@gmail.com>
2019-04-30 22:15:45 +0700

    Changes

--- projects/dqm/create_pipeline.sql
+++ projects/dqm/create_pipeline.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE dqm;
 USE dqm;
 
-CREATE TABLE pipeline ( id INT AUTO_INCREMENT, run_number INT NOT NULL, file_seq INT NOT NULL, evio_file_path VARCHAR(512) NOT NULL, batch_id INT, dqm_file_path VARCHAR(512), aggregated TINYINT(1) DEFAULT 0, error_message TEXT, PRIMARY KEY(id) );
+CREATE TABLE pipeline ( id INT AUTO_INCREMENT, run_number INT NOT NULL, file_seq INT NOT NULL, evio_file_path VARCHAR(512) NOT NULL, job_id INT, job_status CHAR(1), dqm_file_path VARCHAR(512), aggregated TINYINT(1) DEFAULT 0, error_message TEXT, PRIMARY KEY(id) );
 
 CREATE USER 'dqm-user'@'localhost' IDENTIFIED BY '12345';
 
