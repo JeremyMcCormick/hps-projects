9ac4ff9b9d6207355f72cae076fa98faec40de67
Jeremy McCormick <jermccormick@gmail.com>
2019-06-19 14:07:51 +0700

    Add options for start script.

--- projects/monitoring-test/start_monapp.sh
+++ projects/monitoring-test/start_monapp.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
+# 
+# Start the monitoring app, with or without config, using local conditions
+# db file if it exists.
+#
+
+DBFILE=hps_conditions.db
+DBPROP=""
+JARFILE=./hps-distribution-bin.jar
+
+if [ -f "$DBFILE" ]; then
+    echo "Using local conditions db file: $DBFILE"
+    DBPROP=-Dorg.hps.conditions.url=jdbc:sqlite:$DBFILE
+fi
+
+if [[ ! -f "$JARFILE" ]]; then
+    echo "ERROR: Missing jar file at $JARFILE"
+    exit
+fi
+
+CMD="java $DBPROP -cp ./hps-distribution-bin.jar org.hps.monitoring.application.Main"
+
+if [ -z "$1" ]; then 
     echo "Starting monitoring app with no config"
-    java -cp ./hps-distribution-bin.jar org.hps.monitoring.application.Main
 else
     if [ -f "$1" ]; then
+        CMD="${CMD} -c $1"
         echo "Starting monitoring app with config $1"
-        java -cp ./hps-distribution-bin.jar org.hps.monitoring.application.Main -c $1
     else
         echo "ERROR: Config prop file $1 does not exist!"
+        exit
     fi
 fi
 
+echo "Starting monitoring app with command: $CMD"
+$CMD
