95fec79c7b4e2744d98c55ead898a9377186ba1b
Jeremy McCormick <jermccormick@gmail.com>
2019-06-19 14:17:51 +0700

    Remove unnecessary script.

--- projects/monitoring-test/monapp_test.sh
+++
@@ -1,2 +0,0 @@
-java -Dorg.hps.conditions.url=jdbc:sqlite:hps_conditions.db -cp ./hps-distribution-bin.jar org.hps.monitoring.application.Main -c settings.prop
-#java -cp ./hps-distribution-4.1-SNAPSHOT-bin.jar org.hps.monitoring.application.Main
