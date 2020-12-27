83db1f7423c5f8834d57919a95778c0491190765
Jeremy McCormick <jermccormick@gmail.com>
2020-12-27 01:02:08 -0600

    Fix dumb mistake in event builder name

--- projects/online-recon-test/station_test.cmd
+++ projects/online-recon-test/station_test.cmd
@@ -1,24 +1,51 @@
+######################################################
+# Example online recon station settings for 2016 data
+######################################################
+
+# lcsim detector name for conditions
 prop-set lcsim.detector HPS-PhysicsRun2016-Pass2
+
+# lcsim steering resource
 prop-set lcsim.steering /org/hps/steering/recon/PhysicsRun2016FullRecon_RemoteAida.lcsim
-#prop-set lcsim.steering file:///work/slac/git/hps-projects/projects/online-recon-test/Dummy.lcsim
-prop-set lcsim.run 7799
+
+# run number for conditions
+prop-set lcsim.run 7798
+
+# local conditions database file
 prop-set lcsim.conditions jdbc:sqlite:/work/slac/data/conditions/hps_conditions_2020_08_17.db
-prop-set lcsim.maxEvents 10000
-prop-set lcsim.freeze true
-prop-set lcsim.builder org.hps.evio.LCSimTestRunEventBuilder
-prop-set station.queue 1 
-prop-set station.stopOnErrors true
-prop-set station.stopOnEndRun true
-prop-set station.printInterval 1
-prop-set station.loggingConfig ../../station_logging.properties
+
+# event builder
+prop-set lcsim.builder org.hps.evio.LCSimEngRunEventBuilder
+
+# logging file (default uses class resource)
+#prop-set station.loggingConfig ../../station_logging.properties
+
+# name of ET buffer file
 prop-set et.buffer /tmp/ETBuffer
+
+# name of ET host
 prop-set et.host localhost
+
+# ET connection port
 prop-set et.port 11111
+
+# ET log level
 prop-set et.logLevel 4
+
+# ET wait time (ignored for sleep mode)
 prop-set et.waitTime 0
+
+# ET mode (sleep)
 prop-set et.mode 0
-prop-set et.chunk 1
+
+# ET chunk size (number of events to read at once)
+prop-set et.chunk 20
+
+# ET prescale for the station
 prop-set et.prescale 1
+
+# Number of ET connection attempts to make before quitting
 prop-set et.connectionAttempts 10
 
+# Create a station and start it
 create -s 1
